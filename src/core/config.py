baa0947da090fa13677460747cca27c1d074f3fd
alexey <alexey.letov2001@gmail.com>
2026-07-16 18:17:34 +0300

    Create carcase app

---
+++ src/core/config.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import computed_field
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+class Settings(BaseSettings):
+    BOT_TOKEN: str
+    ADMIN_IDS: str
+
+    DB_NAME: str
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_HOST: str
+    DB_PORT: int
+
+    REDIS_PASSWORD: str
+    REDIS_HOST: str
+    REDIS_PORT: int = 6379
+
+    LOG_LEVEL: str = "DEBUG"
+
+    @computed_field
+    @property
+    def database_url(self) -> str:
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+    
+    @computed_field
+    @property
+    def redis_url(self) -> str:
+        if self.REDIS_PASSWORD:
+            return f"redis://{self.REDIS_PASSWORD}@{self.REDIS_HOST}:{self.REDIS_PORT}/0"
+        else:
+            return f"redis://{self.REDIS_HOST}:{self.REDIS_PORT}/0"
+    
+    model_config = SettingsConfigDict(
+        env_file=BASE_DIR / ".env",
+        env_file_encoding="utf-8",
+        extra="ignore"
+    )
+
+settings = Settings()
---
+++ src/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from psina-bot!")
+
+
+if __name__ == "__main__":
+    main()
