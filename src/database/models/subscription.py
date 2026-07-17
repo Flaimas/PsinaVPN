ee7a3c5e37ab27770849fe28acef73c1688ad480
alexey <alexey.letov2001@gmail.com>
2026-07-17 21:34:09 +0300

    feat: add relationship for table user, change structure table, add id primary key

---
+++ src/database/models/subscription.py
@@ -0,0 +1,13 @@
+from src.database.base import Base
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey
+
+class Subscription(Base):
+    __tablename__ = "subscriptions"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"))
+    sub_url: Mapped[str]
+    is_active: Mapped[bool] = mapped_column(default=True, server_default="true")
+
+    user: Mapped["User"] = relationship(back_populates="subscriptions")
