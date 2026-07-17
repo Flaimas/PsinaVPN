c06c5a15d188d2019e9e86d81dc46f611ef02e7d
alexey <alexey.letov2001@gmail.com>
2026-07-17 21:25:23 +0300

    refactoring: remove commit() for method UserRepository

---
+++ src/database/repositories/user.py
@@ -0,0 +1,38 @@
+from sqlalchemy import select, update
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.ext.asyncio import AsyncSession
+from src.database.models.user import User
+from sqlalchemy.dialects.postgresql import insert
+
+class UserRepository:
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def get_or_create_user(self, telegram_id: int, username: str | None) -> User:
+        """Возвращает пользователя, если его нет — создает"""
+        insert_stmt = (
+            insert(User)
+            .values(telegram_id=telegram_id, username=username)
+        )
+
+        upsert_stmt = (
+            insert_stmt.on_conflict_do_update(
+                index_elements=[User.telegram_id],
+                set_={"username": username}
+            )
+            .returning(User)
+        )
+
+        result = await self.session.execute(upsert_stmt)
+        return result.scalar_one()
+
+    async def update_balance(self, telegram_id: int, new_balance: float) -> bool:
+        """Обновляет баланс пользователя. Возвращает True, если запись была обновлена"""
+        query = (
+            update(User)
+            .where(User.telegram_id == telegram_id)
+            .values(balance=new_balance)
+        )
+        
+        result = await self.session.execute(query)
+        return result.rowcount > 0
