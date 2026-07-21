530c8157a71827d098a5a41a4bc0c24d7e06baa6
alexey <alexey.letov2001@gmail.com>
2026-07-21 16:12:01 +0300

    refactor: refactor methods

--- src/database/repositories/tariff.py
+++ src/database/repositories/tariff.py
@@ -5,30 +5,20 @@ from src.database.models.tariff import Tariff
 class TariffRepository:
     def __init__(self, session: AsyncSession):
         self.session = session
-    
-    def calculate_period_price(self, price: int, period: int) -> tuple[int, int]:
-        price_full_period = (period / 30) * int(price)
-        if period > 60:
-            discount = 0.20
-        elif period > 30:
-            discount = 0.10
-        else:
-            discount = 0
-        
-        return int(price_full_period), int(price_full_period - (discount * price_full_period))
 
-    async def get_tariffs(self, is_active: bool = True) -> list[Tariff]:
-        stmt = (
-            select(Tariff)
-            .where(Tariff.is_active == is_active)
-        )
+    async def get_tariffs(self, is_active: bool | None = True) -> list[Tariff]:
+        stmt = select(Tariff)
+
+        if is_active is not None:
+            stmt = stmt.where(Tariff.is_active == is_active)
+
         result = await self.session.execute(stmt)
         return result.scalars().all()
 
-    async def get_tariff_by_slug(self, slug: str) -> Tariff:
+    async def get_tariff_by_slug(self, slug: str, is_active: bool = True) -> Tariff:
         stmt = (
             select(Tariff)
-            .where(Tariff.is_active == True, Tariff.slug == slug)
+            .where(Tariff.is_active == is_active, Tariff.slug == slug)
         )
         result = await self.session.execute(stmt)
         return result.scalar_one()
--- src/database/repositories/user.py
+++ src/database/repositories/user.py
@@ -17,22 +17,13 @@ class UserRepository:
         result = await self.session.execute(stmt)
         return result.scalar_one_or_none()
 
-    async def get_or_create_user(self, telegram_id: int, username: str | None) -> tuple[User, bool]:
-        """
-        Возвращает кортеж (User, bool).
-        Если пользователь уже был в базе, возвращает (user, False).
-        Если пользователя не было, создает его и возвращает (user, True).
-        """
-        user = await self.get_user_by_tg_id(telegram_id=telegram_id)
-        if user:
-            return user, False
+    async def create_user(self, telegram_id: int, username: str | None) -> None:
+        """Добавляет юзера в БД"""
         user = User(
             telegram_id=telegram_id,
             username=username
         )
         self.session.add(user)
-        await self.session.flush()
-        return user, True
 
     async def update_balance(self, telegram_id: int, new_balance: float) -> bool:
         """Обновляет баланс пользователя. Возвращает True, если запись была обновлена"""
