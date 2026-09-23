1ce3e6a1ac83359d64e8ff97beeaa86e30a89455
Sergay Gutuev <alexey.letov2001@gmail.com>
2026-09-23 23:52:33 +0300

    feat: add referrer_id in users table

--- src/database/models/user.py
+++ src/database/models/user.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from datetime import datetime
 from typing import TYPE_CHECKING
 
-from sqlalchemy import BigInteger, DateTime, func
+from sqlalchemy import BigInteger, DateTime, ForeignKey, func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.core.config import settings
@@ -25,6 +25,9 @@ class User(Base):
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), server_default=func.timezone("utc", func.now())
     )
+    referrer_id: Mapped[int | None] = mapped_column(
+        BigInteger, ForeignKey("users.id", ondelete="SET NULL"), nullable=True
+    )
     balance: Mapped[float] = mapped_column(
         default=settings.START_BALANCE, server_default="0.0", nullable=False
     )
@@ -33,6 +36,10 @@ class User(Base):
     )
     is_test_used: Mapped[bool] = mapped_column(default=False, server_default="false")
 
+    referrer: Mapped[User | None] = relationship(
+        remote_side=[id], back_populates="referrals"
+    )
+    referrals: Mapped[list[User]] = relationship(back_populates="referrer")
     subscription: Mapped[Subscription | None] = relationship(
         back_populates="user", uselist=False
     )
